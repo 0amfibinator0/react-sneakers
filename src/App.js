dfeffddc2cead6448ecfaef809bbf6f536371ad1
Deni <00amfibinator00@mail.ru>
2022-07-29 01:53:19 +0300

    урок 5.1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios'
 import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
@@ -6,36 +7,50 @@ import Drawer from './components/Drawer';
 function App() {
   const [items, setItems] = React.useState([]);
   const [cartItems, setCartItems] = React.useState([]);
+  const [searchValue, setSearchValue] = React.useState('');
   const [cartOpened, setCartOpened] = React.useState(false);
 
   React.useEffect(() => {
-    fetch('https://62e16e35fa99731d75d66e03.mockapi.io/items')
-      .then(res => {
-        return res.json()
-      })
-      .then((json) => {
-        setItems(json);
-      })
-  }, []);
+    axios.get('https://62e16e35fa99731d75d66e03.mockapi.io/items')
+      .then((res) => {
+        setItems(res.data);
+      });
+    axios.get('https://62e16e35fa99731d75d66e03.mockapi.io/cart')
+      .then((res) => {
+        setCartItems(res.data);
+      });
+    }, []);
 
   const onAddToCart = (obj) => {
+    axios.post('https://62e16e35fa99731d75d66e03.mockapi.io/cart', obj);
     setCartItems((prev) => [...prev, obj]);
   }
 
+  const onRemoveItem = (id) => {
+    axios.delete(`https://62e16e35fa99731d75d66e03.mockapi.io/cart/${id}`);
+    setCartItems((prev) => prev.filter(item => item.id !== id));
+  }
+
+  const onChangeSearchInput = (event) => {
+    setSearchValue(event.target.value)
+  }
+
   return <div className="wrapper clear">
-    {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} />}
+    {cartOpened && <Drawer items={cartItems} onClose={() => setCartOpened(false)} onRemove={onRemoveItem} />}
     <Header onClickCart={() => setCartOpened(true)}/>
     <div className="content p-40">
       <div className="d-flex align-center mb-40 justify-between">
-        <h1 className="">Все кроссовки</h1>
+        <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
         <div className="search-block d-flex">
           <img src="/img/search.svg" alt="Search" />
-          <input placeholder="Поиск..." />
+          {searchValue && <img onClick={() => setSearchValue('')} className="clear cu-p" src="/img/btn-remove.svg" alt="clear" />}
+          <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..." />
         </div>
       </div>
       <div className="d-flex flex-wrap">
-        {items.map((item) => (
+        {items.filter((item) => item.title.toLowerCase().includes(searchValue.toLowerCase())).map((item, index) => (
           <Card
+            key={index}
             title={item.title}
             price={item.price}
             imageUrl={item.imageUrl}
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ function Header(props) {
   return (
     <header header className="d-flex justify-between align-center p-40">
       <div className="d-flex align-center">
-        <img className="logo" src="/img/logo.png" alt="" />
+        <img className="logo" src="/img/logo.png" alt="logo" />
         <div>
           <h3 className="text-uppercase"> React Sneakers</h3>
           <p className="opacity-5"> Магазин лучших кроссовок</p>
@@ -10,11 +10,11 @@ function Header(props) {
       </div>
       <ul className="d-flex">
         <li className="mr-30 d-flex align-center cu-p" onClick={props.onClickCart}>
-          <img width={18} height={18} src="/img/cart.svg" alt="" />
+          <img width={18} height={18} src="/img/cart.svg" alt="cart" />
           <span>1205 руб.</span>
         </li>
         <li>
-          <img width={18} height={18} src="/img/user.svg" alt="" />
+          <img width={18} height={18} src="/img/user.svg" alt="user" />
         </li>
       </ul>
     </header>
